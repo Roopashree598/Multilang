c46aae00d590c1a76ee5e575218e34cb3bab6642
sroopashree <sroopashree@betatestsolutions.com>
2023-06-20 17:21:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import logo from './logo.svg';
+import React, {useContext} from 'react';
 import './App.css';
+import {FormattedMessage, FormattedDate } from 'react-intl';
+import {Context} from "./components/Wrapper";
+import ReactPlayer from 'react-player'
 
-function App() {
+function App(props) {
+  const context = useContext(Context);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <select value = {context.locale} onChange={context.selectLanguage}>
+          <option value= 'en'>English</option>
+          <option value= 'fr'>French</option>
+          <option value= 'ar'>Arabic</option>
+
+        </select>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <FormattedMessage
+              id = "app.header"
+              defaultMessage="React internalization"
+              values = {{fileName: 'src/App.js', code: (word)=> <strong>{word}</strong>}}
+          />
         </p>
         <a
           className="App-link"
@@ -15,9 +29,27 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FormattedMessage
+            id = "app.content"
+            defaultMessage="Learning how to render multilang using react internalization"
+          />
+
         </a>
+        <FormattedMessage
+          id = "app.subHeading"
+          defaultMessage="multilanguage"
+        />
+        <br/>
+        <FormattedDate
+            value={props.date}
+            year = 'numeric'
+            month= 'long'
+            day = 'numeric'
+            weekday = 'long'
+        />
+        <br/>
       </header>
+      <div><ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' /></div>
     </div>
   );
 }
